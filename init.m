7c4483a6008ee36e2a401a26c0660f17bb504ec8
QuangDung <hoquangdung2003@gmail.com>
2025-09-05 21:08:02 +0700

    first commit

---
+++ init.m
@@ -0,0 +1,15 @@
+g = 9.8; %m/s^2
+AUM = 3.057; %kg
+Ixx = 49827; %kg mm^2
+Iyy = 48993; %kg mm^2
+Izz = 82777; %kg mm^2
+Ixy = 12; %kg mm^2
+Ixz = 35; %kg mm^2
+Iyz = 295; %kg mm^2
+Ct = 0.013;
+Cq = 0.013;
+L = 0.25;           % Chiều dài arm (m)
+H = 0.05;           % Chiều cao hub (m)
+rho = 1.18195;
+R = 152.5; %mm
+R_rad = 0.5;
---
+++ weighted_pseudoinverse.m
@@ -0,0 +1,14 @@
+function u = weighted_pseudoinverse(v, B, W)
+    % Input: v (4x1), B (4x12), W (12x12 diagonal)
+    % Output: u (12x1)
+    
+    % Tính weighted pseudoinverse
+    % Winv = inv(W); % Ma trận nghịch đảo trọng số
+    % P_w = W \ B' * pinv(B *W \ B'); % Sử dụng pinv để ổn định
+    % u = P_w * v;
+
+    u = W \ B' * ((B / W * B') \ v);
+    
+    % Xử lý ràng buộc 
+    % u = max(-1, min(1, u));
+end
