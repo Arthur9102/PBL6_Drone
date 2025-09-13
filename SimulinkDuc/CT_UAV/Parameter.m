e0e91c6c4583937922d9caf6b2b96e1fa9000c9b
Nguyen Duc <nguyenphananhduc2212@gmail.com>
2025-09-13 13:50:47 +0700

    them quy dao cho Simulink

--- SimulinkDuc/CT_UAV/Parameter.m
+++ SimulinkDuc/CT_UAV/Parameter.m
@@ -1,20 +1,22 @@
-g = -9.8; %m/s^2
+clear
+clc
+g = 10; %m/s^2
 AUM = 3.057; %kg
-Ixx = 0.49827; %kg mm^2
-Iyy = 0.48993; %kg mm^2
-Izz = 0.82777; %kg mm^2
+Ixx = 0.049827; %kg mm^2
+Iyy = 0.048993; %kg mm^2
+Izz = 0.082777; %kg mm^2
 Ixy = 0.12; %kg mm^2
 Ixz = 0.35; %kg mm^2
 Iyz = 0.295; %kg mm^2
 Ct = 0.013;
 Cq = 0.013;
-L = 0.25;           % Chiều dài arm (m)
+L = 0.3;           % Chiều dài arm (m)
 H = 0.05;           % Chiều cao hub (m)
 rho = 1.18195;
-R = 152.5; %mm
+R = 0.1525; %mm
 W = diag([0.994, 0.003, 0.003, 0.994, 0.003, 0.003, ...
           0.994, 0.003, 0.003, 0.994, 0.003, 0.003]);
-R_rad = 0.15;
+R_rad = 0.1905;
 
 % uavthamso.geom.g = 10;
 % uavthamso.aero.Ct = 0.013;
